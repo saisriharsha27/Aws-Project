ca0acff6abaf8d6a8e1dc518fbdde4b61057936e
saisriharsha27 <smankala@my.bridgeport.edu>
2022-04-09 21:30:32 -0400

    added test function e

--- lambdafunction.py
+++ lambdafunction.py
@@ -50,6 +50,7 @@ def lambda_handler(event, context):
         return "hello"
     except Exception as e:
         print(e)
+        print("e")
         print("Error processing object {} from bucket {}. ".format(key, bucket) +
               "Make sure your object and bucket exist and your bucket is in the same region as this function.")
         raise e
